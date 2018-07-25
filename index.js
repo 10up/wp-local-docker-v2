c7026e5cbd79b9598b688c4af230edd4ccafcf1a
Chris Marslender <chrismarslender@gmail.com>
2018-07-25 14:04:42 -0700

    Add prompts for phpfpm and elasticsearch

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var yaml = require( 'write-yaml' );
+var prompt = require( 'prompt' );
 
 var baseConfig = {
 	'version': '3',
@@ -54,6 +55,90 @@ var baseConfig = {
 	}
 };
 
+prompt.start();
+
+var schema = {
+	properties: {
+		phpVersion: {
+			description: "What version of PHP would you like to use? [5.5, 5.6, 7.0, 7.1, 7.2]",
+			message: "You must select one of 5.5, 5.6, 7.0, 7.1, or 7.2",
+			type: 'string',
+			required: true,
+			default: '7.2',
+			enum: [ '5.5', '5.6', '7.0', '7.1', '7.2' ],
+		},
+		elasticsearch: {
+			description: "Do you need Elasticsearch? (Y/n)",
+			//type: 'boolean', // Doesn't allow Y/n
+			message: "You must choose either `Y` or `n`",
+			type: 'string',
+			required: true,
+			default: 'Y',
+			enum: [ 'Y', 'y', 'N', 'n' ],
+			before: function( value ) {
+				var y = new RegExp( /^y(es)?$/i );
+				var n = new RegExp( /^no?$/i );
+
+				if ( typeof value !== 'string' ) {
+					return value;
+				}
+
+				if ( value.match( y ) !== null ) {
+					return 'true';
+				} else if ( value.match( n ) !== null ) {
+					return 'false';
+				}
+
+				return value;
+			},
+		},
+	},
+};
+
+prompt.get( schema, function( err, result ) {
+	baseConfig.services.phpfpm = {
+		'image': '10up/phpfpm:' + result.phpVersion,
+		'restart': 'unless-stopped',
+		'volumes': [
+			'./wordpress:/var/www/html',
+			'./config/php-fpm/php.ini:/usr/local/etc/php/php.ini',
+			'./config/php-fpm/docker-php-ext-xdebug.ini:/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini',
+			'./config/php-fpm/wp-cli.local.yml:/var/www/html/wp-cli.local.yml',
+			'~/.ssh:/root/.ssh'
+		],
+		'depends_on': [
+			'mysql',
+			'memcached',
+		]
+	};
+
+	if ( result.elasticsearch === 'true' ) {
+		baseConfig.services.phpfpm.depends_on.push( 'elasticsearch' );
+
+		baseConfig.services.elasticsearch = {
+			image: 'docker.elastic.co/elasticsearch/elasticsearch:5.6.5',
+			'restart': 'unless-stopped',
+			'ports': [
+				'9200:9200'
+			],
+			'volumes': [
+				'./config/elasticsearch/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml',
+				'./config/elasticsearch/plugins:/usr/share/elasticsearch/plugins'
+			],
+			'environment': {
+				ES_JAVA_OPTS: '-Xms750m -Xmx750m'
+			}
+
+		};
+	}
+
+	yaml( 'docker-compose.yml', baseConfig, { 'lineWidth': 500 }, function( err ) {
+		console.log(err);
+	});
+
+});
+
+
 // prompt:
 // phpfpm version
 // elasticsearch
@@ -72,7 +157,4 @@ var baseConfig = {
 
 // Add update checker?
 
-yaml( 'docker-compose.yml', baseConfig, function( err ) {
-	console.log(err);
-});
 
