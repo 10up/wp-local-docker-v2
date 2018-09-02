13cbea82d5bc4301e0060a7e74d8ee6419b13893
Chris Marslender <chrismarslender@gmail.com>
2018-09-01 20:49:24 -0700

    Add a configure command to enable the generator to live separately from the environments. Fix a bunch of things that now need to be async to work with async FS operations

--- src/wordpress.js
+++ src/wordpress.js
@@ -2,15 +2,15 @@ const execSync = require('child_process').execSync;
 const envUtils = require( './env-utils' );
 
 
-const download = function( env ) {
-    let envPath = envUtils.envPath( env );
+const download = async function( env ) {
+    let envPath = await envUtils.envPath( env );
 
     console.log( "Downloading WordPress" );
     execSync( `cd ${envPath} && docker-compose exec phpfpm su -s /bin/bash www-data -c "wp core download --force"`, { stdio: 'inherit' });
 };
 
-const downloadDevelop = function( env ) {
-    let envPath = envUtils.envPath( env );
+const downloadDevelop = async function( env ) {
+    let envPath = await envUtils.envPath( env );
 
     console.log( "Downloading WordPress Develop" );
     execSync( `cd ${envPath} && docker-compose exec phpfpm su -s /bin/bash www-data -c "git clone git://develop.git.wordpress.org/ ."`, { stdio: 'inherit' });
@@ -18,16 +18,16 @@ const downloadDevelop = function( env ) {
     execSync( `cd ${envPath} && docker run -t --rm -v ${envPath}/wordpress:/usr/src/app 10up/wpcorebuild:latest grunt`, { stdio: 'inherit' });
 };
 
-const configure = function( env ) {
+const configure = async function( env ) {
     let envSlug = envUtils.envSlug( env );
-    let envPath = envUtils.envPath( env );
+    let envPath = await envUtils.envPath( env );
 
     console.log( "Configuring WordPress" );
     execSync( `cd ${envPath} && docker-compose exec phpfpm su -s /bin/bash www-data -c "wp config create --force --dbname=${envSlug}"`, { stdio: 'inherit' });
 };
 
-const install = function( env, envHost ) {
-    let envPath = envUtils.envPath( env );
+const install = async function( env, envHost ) {
+    let envPath = await envUtils.envPath( env );
 
     execSync( `cd ${envPath} && docker-compose exec phpfpm su -s /bin/bash www-data -c "wp core install --url=http://${envHost} --prompt=title,admin_user,admin_password,admin_email"`, { stdio: 'inherit' });
 };
@@ -35,26 +35,26 @@ const install = function( env, envHost ) {
 // Splitting these out, because --prompt=subdomains doesn't actually spit out that prompt for some reason
 // @todo file bug report on wp-cli for this
 // Fair warning... Once this is fixed in wp-cli, these two functions will be consolidated to one installMultisite, with a prompt for --subdirectories in wp-cli
-const installMultisiteSubdirectories = function( env, envHost ) {
-    let envPath = envUtils.envPath( env );
+const installMultisiteSubdirectories = async function( env, envHost ) {
+    let envPath = await envUtils.envPath( env );
 
     execSync( `cd ${envPath} && docker-compose exec phpfpm su -s /bin/bash www-data -c "wp core multisite-install --url=http://${envHost} --prompt=title,admin_user,admin_password,admin_email"`, { stdio: 'inherit' });
 };
 
-const installMultisiteSubdomains = function( env, envHost ) {
-    let envPath = envUtils.envPath( env );
+const installMultisiteSubdomains = async function( env, envHost ) {
+    let envPath = await envUtils.envPath( env );
 
     execSync( `cd ${envPath} && docker-compose exec phpfpm su -s /bin/bash www-data -c "wp core multisite-install --url=http://${envHost} --subdomains --prompt=title,admin_user,admin_password,admin_email"`, { stdio: 'inherit' });
 };
 
-const setRewrites = function( env ) {
-    let envPath = envUtils.envPath( env );
+const setRewrites = async function( env ) {
+    let envPath = await envUtils.envPath( env );
 
     execSync( `cd ${envPath} && docker-compose exec phpfpm su -s /bin/bash www-data -c "wp rewrite structure /%postname%/"`, { stdio: 'inherit' });
 };
 
-const emptyContent = function( env ) {
-    let envPath = envUtils.envPath( env );
+const emptyContent = async function( env ) {
+    let envPath = await envUtils.envPath( env );
 
     execSync( `cd ${envPath} && docker-compose exec phpfpm su -s /bin/bash www-data -c "wp site empty --yes"`, { stdio: 'inherit' });
     execSync( `cd ${envPath} && docker-compose exec phpfpm su -s /bin/bash www-data -c "wp plugin delete hello akismet"`, { stdio: 'inherit' });
--- src/wpsnapshots.js
+++ src/wpsnapshots.js
@@ -4,20 +4,22 @@ const path = require( 'path' );
 const execSync = require('child_process').execSync;
 const envUtils = require('./env-utils');
 const gateway = require( './gateway' );
+const config = require( './configure' );
 
 const command = async function() {
     // false catches the case when no subcommand is passed, and we just pass to snapshots to show usage
-    let bypassCommands = [ false, 'configure', 'help', 'list' ];
-    let noPathCommands = [ false, 'configure', 'help', 'list', 'delete', 'search', 'download' ];
+    let bypassCommands = [ undefined, 'configure', 'help', 'list' ];
+    let noPathCommands = [ undefined, 'configure', 'help', 'list', 'delete', 'search', 'download' ];
     let envPath = false;
 
     // Ensure that the wpsnapshots folder is created and owned by the current user versus letting docker create it so we can enforce proper ownership later
-    await fs.ensureDir( path.join( envUtils.globalPath, 'wpsnapshots' ) );
+    let wpsnapshotsDir = path.join( config.getConfigDirectory(), 'wpsnapshots' );
+    await fs.ensureDir( wpsnapshotsDir );
 
     // Except for a few whitelisted commands, enforce a configuration before proceeding
     if ( bypassCommands.indexOf( commandUtils.subcommand() ) === -1 ) {
         // Verify we have a configuration
-        if ( fs.existsSync( path.join( envUtils.globalPath, 'wpsnapshots', 'config.json' ) ) === false ) {
+        if ( fs.existsSync( path.join( wpsnapshotsDir, 'config.json' ) ) === false ) {
             console.error( "Error: WP Snapshots does not have a configuration file. Please run '10updocker wpsnapshots configure' before continuing." );
             process.exit();
         }
@@ -26,12 +28,12 @@ const command = async function() {
     // These commands can be run without being in the context of a WP install
     if ( noPathCommands.indexOf( commandUtils.subcommand() ) === -1 ) {
         // @todo allow users to specify environment an alternate way
-        let envSlug = envUtils.parseEnvFromCWD();
+        let envSlug = await envUtils.parseEnvFromCWD();
         if ( envSlug === false ) {
             console.error( "Error: Unable to determine which environment to use wp snapshots with. Please run this command from within your environment." );
             process.exit(1);
         }
-        envPath = envUtils.envPath( envSlug );
+        envPath = await envUtils.envPath( envSlug );
     }
 
     // Get everything after the snapshots command, so we can pass to the docker container
@@ -40,10 +42,10 @@ const command = async function() {
     // @todo update the image version once new images are merged
     try{
         if ( envPath === false ) {
-            execSync( `docker run -it --rm -v ${envUtils.globalPath}/wpsnapshots:/home/wpsnapshots/.wpsnapshots 10up/wpsnapshots:dev ${command}`, { stdio: 'inherit' });
+            execSync( `docker run -it --rm -v ${wpsnapshotsDir}:/home/wpsnapshots/.wpsnapshots 10up/wpsnapshots:dev ${command}`, { stdio: 'inherit' });
         } else {
             await gateway.startGlobal();
-            execSync( `docker run -it --rm --network wplocaldocker -v ${envPath}/wordpress:/var/www/html -v ${envUtils.globalPath}/wpsnapshots:/home/wpsnapshots/.wpsnapshots 10up/wpsnapshots:dev ${command}`, { stdio: 'inherit' });
+            execSync( `docker run -it --rm --network wplocaldocker -v ${envPath}/wordpress:/var/www/html -v ${wpsnapshotsDir}:/home/wpsnapshots/.wpsnapshots 10up/wpsnapshots:dev ${command}`, { stdio: 'inherit' });
         }
     } catch (ex) {}
 };
