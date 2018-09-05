2863d68e4004557cedf5275667b89661b8031a4f
Chris Marslender <chrismarslender@gmail.com>
2018-09-04 19:38:06 -0700

    Clear wpsnapshots cache when clearing cache

--- src/cache.js
+++ src/cache.js
@@ -1,7 +1,10 @@
+const path = require( 'path' );
+const fs = require( 'fs-extra' );
 const commandUtils = require( './command-utils' );
 const gateway = require( './gateway' );
+const config = require( './configure' );
+const async = require( 'asyncro' );
 
-// @todo clear wp-snapshots cache. Hoping for alternate directory structure
 const help = function() {
     let help = `
 Usage: 10updocker cache clear
@@ -15,6 +18,22 @@ Clears npm, wp-cli, and WP Snapshots caches
 const clear = async function() {
     await gateway.removeCacheVolume();
     await gateway.ensureCacheExists();
+
+    let wpsnapshotsDir = path.join( config.getConfigDirectory(), 'wpsnapshots' );
+    let snapDirContent = await fs.readdir( wpsnapshotsDir );
+
+    // Filter to only directories. Makes sure we don't delete the config
+    snapDirContent = await async.filter( snapDirContent, async item => {
+        let testPath = path.join( wpsnapshotsDir, item );
+        let stat = await fs.stat( testPath );
+
+        return stat.isDirectory();
+    });
+
+    await async.map( snapDirContent, async item => {
+        await fs.remove( path.join( wpsnapshotsDir, item ) );
+    });
+
     console.log( "Cache Cleared" );
 };
 
