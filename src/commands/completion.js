1a4b83d9aaf94a38b1c861d740b8e7256d8150df
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-07-18 08:58:23 +0300

    Added completion command.

---
+++ src/commands/completion.js
@@ -0,0 +1,31 @@
+const { resolve } = require( 'path' );
+
+const chalk = require( 'chalk' );
+
+exports.command = 'completion <shell>';
+exports.desc = 'Displays completion script for selected shell';
+
+exports.builder = function( yargs ) {
+    yargs.positional( 'shell', {
+        describe: 'A shell to display completion script for.',
+        type: 'string',
+    } );
+};
+
+exports.handler = function( { shell } ) {
+    switch( shell ) {
+        case 'bash': {
+            const filename = resolve( __dirname, '..', 'scripts', '10updocker-completion.bash' );
+            console.log( `source ${filename}` );
+            break;
+        }
+        case 'zsh':
+            console.error( chalk.red( 'ZSH is not supported yet.' ) );
+            process.exit( 2 );
+            break;
+        default:
+            console.error( `${ chalk.bold.redBright( shell ) } ${ chalk.red( 'shell is not supported.' ) }` );
+            process.exit( 1 );
+            break;
+    }
+};
