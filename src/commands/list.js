7fc1e95c7ef9e61b53a873b2b8dc42d248b69f8a
thrijith <thrijith13@gmail.com>
2020-08-03 14:54:01 +0530

    Check Docker availability before checking for containers

--- src/commands/list.js
+++ src/commands/list.js
@@ -19,34 +19,44 @@ exports.command = 'list';
 exports.desc = 'Lists all the environments and meta information.';
 
 exports.handler = makeCommand( chalk, logSymbols, async () => {
-    // Get all the environments and initialize a status array.
-    const environments = await envUtils.getAllEnvironments();
-    const envStatus = [ [ 'Name', 'Status', 'URL' ] ];
-    const docker = makeDocker();
+    try{
+        // Create docker object and make sure it is available.
+        const docker = makeDocker();
+        const dockerStatus = await docker.info();
 
-    // Loop through each environment and add details.
-    for ( const envSlug of environments ) {
-        // Get path of the current environment to perform dedicated checks.
-        const envPath = await envUtils.envPath( envSlug );
+        // If docker is running, go ahead and check container status for each environment.
+        if ( typeof dockerStatus.ID !== 'undefined' ) {
+            // Get all the environments and initialize a status array.
+            const environments = await envUtils.getAllEnvironments();
+            const envStatus = [ [ 'Name', 'Status', 'URL' ] ];
 
-        // Get current environment host name, use the starting index.
-        const envHosts = await envUtils.getEnvHosts( envPath );
-        const hostName = `http://${ envHosts[0] }/`;
+            // Loop through each environment and add details.
+            for ( const envSlug of environments ) {
+                // Get path of the current environment to perform dedicated checks.
+                const envPath = await envUtils.envPath( envSlug );
 
-        try {
-            const containers = await docker.listContainers( { filters: { 'name': [ envSlug ] } } );
+                // Get current environment host name, use the starting index.
+                const envHosts = await envUtils.getEnvHosts( envPath );
+                const hostName = `http://${ envHosts[0] }/`;
 
-            // Check containers availability and push to list with appropriate status.
-            if ( Array.isArray( containers ) && containers.length ) {
-                envStatus.push( [ envSlug, 'UP', terminalLink( chalk.cyanBright( hostName ), hostName ) ] );
-            } else {
-                envStatus.push( [ envSlug, 'DOWN', terminalLink( chalk.cyanBright( hostName ), hostName ) ] );
+                try {
+                    const containers = await docker.listContainers( { filters: { 'name': [ envSlug ] } } );
+
+                    // Check containers availability and push to list with appropriate status.
+                    if ( Array.isArray( containers ) && containers.length ) {
+                        envStatus.push( [ envSlug, 'UP', terminalLink( chalk.cyanBright( hostName ), hostName ) ] );
+                    } else {
+                        envStatus.push( [ envSlug, 'DOWN', terminalLink( chalk.cyanBright( hostName ), hostName ) ] );
+                    }
+                } catch( ex ) {
+                    console.error( ex );
+                }
             }
-        } catch( ex ) {
-            console.error( ex );
-        }
-    }
 
-    // Output the environment status.
-    console.log( table( envStatus ) );
+            // Output the environment status.
+            console.log( table( envStatus ) );
+        }
+    } catch( ex ) {
+        console.error( chalk.red( 'Docker is not running...' ) );
+    }
 } );
