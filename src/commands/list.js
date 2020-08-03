ca64ef09f3016c55e1fc19494e24c9eae0e46bf1
thrijith <thrijith13@gmail.com>
2020-08-04 00:27:51 +0530

    Update makeCommand to add a ping check for Docker Update list command to adapt to change of makeCommand

--- src/commands/list.js
+++ src/commands/list.js
@@ -3,7 +3,6 @@
  */
 const { table } = require( 'table' );
 const chalk = require( 'chalk' );
-const logSymbols = require( 'log-symbols' );
 const terminalLink = require( 'terminal-link' );
 
 /**
@@ -18,45 +17,36 @@ exports.aliases = [ 'ls' ];
 exports.command = 'list';
 exports.desc = 'Lists all the environments and meta information.';
 
-exports.handler = makeCommand( chalk, logSymbols, async () => {
-    try{
-        // Create docker object and make sure it is available.
-        const docker = makeDocker();
-        const dockerStatus = await docker.info();
+exports.handler = makeCommand( {}, {}, async () => {
+    // Create docker object and make sure it is available.
+    const docker = makeDocker();
+    // Get all the environments and initialize a status array.
+    const environments = await envUtils.getAllEnvironments();
+    const envStatus = [ [ 'Name', 'Status', 'URL' ] ];
 
-        // If docker is running, go ahead and check container status for each environment.
-        if ( typeof dockerStatus.ID !== 'undefined' ) {
-            // Get all the environments and initialize a status array.
-            const environments = await envUtils.getAllEnvironments();
-            const envStatus = [ [ 'Name', 'Status', 'URL' ] ];
+    // Loop through each environment and add details.
+    for ( const envSlug of environments ) {
+        // Get path of the current environment to perform dedicated checks.
+        const envPath = await envUtils.envPath( envSlug );
 
-            // Loop through each environment and add details.
-            for ( const envSlug of environments ) {
-                // Get path of the current environment to perform dedicated checks.
-                const envPath = await envUtils.envPath( envSlug );
+        // Get current environment host name, use the starting index.
+        const envHosts = await envUtils.getEnvHosts( envPath );
+        const hostName = `http://${ envHosts[0] }/`;
 
-                // Get current environment host name, use the starting index.
-                const envHosts = await envUtils.getEnvHosts( envPath );
-                const hostName = `http://${ envHosts[0] }/`;
+        try {
+            const containers = await docker.listContainers( { filters: { 'name': [ envSlug ] } } );
 
-                try {
-                    const containers = await docker.listContainers( { filters: { 'name': [ envSlug ] } } );
-
-                    // Check containers availability and push to list with appropriate status.
-                    if ( Array.isArray( containers ) && containers.length ) {
-                        envStatus.push( [ envSlug, 'UP', terminalLink( chalk.cyanBright( hostName ), hostName ) ] );
-                    } else {
-                        envStatus.push( [ envSlug, 'DOWN', terminalLink( chalk.cyanBright( hostName ), hostName ) ] );
-                    }
-                } catch( ex ) {
-                    console.error( ex );
-                }
+            // Check containers availability and push to list with appropriate status.
+            if ( Array.isArray( containers ) && containers.length ) {
+                envStatus.push( [ envSlug, 'UP', terminalLink( chalk.cyanBright( hostName ), hostName ) ] );
+            } else {
+                envStatus.push( [ envSlug, 'DOWN', terminalLink( chalk.cyanBright( hostName ), hostName ) ] );
             }
-
-            // Output the environment status.
-            console.log( table( envStatus ) );
+        } catch( ex ) {
+            console.error( ex );
         }
-    } catch( ex ) {
-        console.error( chalk.red( 'Docker is not running...' ) );
     }
+
+    // Output the environment status.
+    console.log( table( envStatus ) );
 } );
--- src/utils/make-command.js
+++ src/utils/make-command.js
@@ -1,7 +1,34 @@
 const { EOL } = require( 'os' );
+const chalk = require( 'chalk' );
+const logSymbols = require( 'log-symbols' );
+const makeDocker = require( '../utils/make-docker' );
 
-module.exports = function makeCommand( chalk, { error }, command ) {
+/**
+ * Check if Docker app is running.
+ *
+ * @return {Promise<boolean|*>}
+ */
+async function pingDocker() {
+    try{
+        const docker = makeDocker();
+        return await docker.ping();
+    } catch( ex ) {
+        return false;
+    }
+}
+
+module.exports = function makeCommand( options = {}, { error }, command ) {
+    const { checkDocker = true } = options;
     return ( ...params ) => {
+        // Check if Docker is running.
+        if ( checkDocker === true ) {
+            pingDocker().then( result => {
+                if ( result === false ) {
+                    console.log( logSymbols.error, chalk.red( 'Docker is not running...' ) );
+                    process.exit();
+                }
+            } );
+        }
         return command( ...params ).catch( ( err ) => {
             process.stderr.write( chalk.red( `${ EOL }${ error } ${ err.message || 'Unexpected error' }${ EOL }${ EOL }` ) );
             process.exit( 1 );
