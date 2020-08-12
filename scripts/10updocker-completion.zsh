60071c6fa1db6247a28a8de2ac38384e593f0f08
thrijith <thrijith13@gmail.com>
2020-08-12 20:03:41 +0530

    Add `zsh` completion using script generated by yargs

---
+++ scripts/10updocker-completion.zsh
@@ -0,0 +1,17 @@
+###-begin-10updocker-completions-###
+#
+# yargs command completion script
+#
+# Installation: 10updocker completion zsh >> ~/.zshrc
+#
+_10updocker_yargs_completions()
+{
+  local reply
+  local si=$IFS
+  IFS=$'
+' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" 10updocker --get-yargs-completions "${words[@]}"))
+  IFS=$si
+  _describe 'values' reply
+}
+compdef _10updocker_yargs_completions 10updocker
+###-end-10updocker-completions-###
