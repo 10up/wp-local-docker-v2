891297267e49c336669329b24ecc22ad3567aceb
thrijith <thrijith13@gmail.com>
2020-08-13 13:10:39 +0530

    Add `zsh` completion for `10updocker-hosts`

--- scripts/10updocker-completion.zsh
+++ scripts/10updocker-completion.zsh
@@ -15,3 +15,21 @@ _10updocker_yargs_completions()
 }
 compdef _10updocker_yargs_completions 10updocker
 ###-end-10updocker-completions-###
+
+###-begin-10updocker-hosts-completions-###
+#
+# yargs command completion script
+#
+# Installation: 10updocker-hosts completion >> ~/.zshrc
+#
+_10updocker-hosts_yargs_completions()
+{
+  local reply
+  local si=$IFS
+  IFS=$'
+' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" 10updocker-hosts --get-yargs-completions "${words[@]}"))
+  IFS=$si
+  _describe 'values' reply
+}
+compdef _10updocker-hosts_yargs_completions 10updocker-hosts
+###-end-10updocker-hosts-completions-###
