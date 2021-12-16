65ced78eda7087c3a86a6c9fa039a6f13db18018
Tyler Cherpak <tylertcherpak@gmail.com>
2021-12-15 20:56:54 -0500

    adding some doc blocks

--- src/env-utils.js
+++ src/env-utils.js
@@ -233,6 +233,12 @@ function createDefaultProxy( value ) {
 	return proxyUrl;
 }
 
+/**
+ * Gets the PHP version of an env from the docker-compose.yml.
+ *
+ * @param {string} envPath path to the environemt
+ * @return string php version
+ */
 async function getEnvPhpVersion( envPath ) {
 	const dockerCompose = path.join( envPath, 'docker-compose.yml' );
 	const yaml = readYaml( dockerCompose );
@@ -240,6 +246,11 @@ async function getEnvPhpVersion( envPath ) {
 	return phpVersion.split( '-' ).shift();
 }
 
+/**
+ * Compares PHP version and desplays warning if the version is EOL.
+ *
+ * @param {string} phpVersion the php version to compare
+ */
 async function checkForEOLPHP( phpVersion ) {
 	if ( endOfLifePhpVersions.includes( phpVersion ) ) {
 		console.error( `${ chalk.bold.yellow( 'Warning:' ) } This environment is using an outdated version of PHP. Please update as soon as possible.` );
