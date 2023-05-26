6e618f7056823edd3a05bc96b4a135be3bf4efe1
Daryll Doyle <daryll.doyle@10up.com>
2023-05-26 12:28:15 +0100

    Add newer php versions for selection
    Closes #332
    Closes #316
    
    Supersedes #319

--- src/docker-images.js
+++ src/docker-images.js
@@ -7,6 +7,8 @@ exports.globalImages = {
 };
 
 exports.images = {
+	'php8.2': '10up/wp-php-fpm-dev:8.2-ubuntu',
+	'php8.1': '10up/wp-php-fpm-dev:8.1-ubuntu',
 	'php8.0': '10up/wp-php-fpm-dev:8.0-ubuntu',
 	'php7.4': '10up/wp-php-fpm-dev:7.4-ubuntu',
 	'php7.3': '10up/wp-php-fpm-dev:7.3-ubuntu',
